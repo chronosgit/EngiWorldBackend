2121ad069fb763cabb07778aeb7de02307a5b587
chronosgit <warfacemeister@mail.ru>
2023-10-15 15:48:31 +0600

    Added auth post requests and User model

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
+const jwt = require("jsonwebtoken");
 require('dotenv').config();
 
-const loginUser = require("./routes/login");
 const refreshToken = require("./routes/refresh");
 const cookieJwtAuth = require("./middleware/bearerTokenAuth");
+
+const Models = require("./models");
 
 mongoose.connect("mongodb://127.0.0.1:27017/engiworld");
 
@@ -12,16 +15,55 @@ const app = express();
 const PORT = "3001";
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 
 
-loginUser(app); // login function from routes/login.js
 refreshToken(app); // to refresh tokens
 
 app.listen(PORT, (req, res) => {
     console.log(`Server is listening on port ${PORT}`);
 });
 
-app.get("/data", cookieJwtAuth, (req, res) => {
-    res.json({msg: req.user});
+app.post("/auth/login/", async (req, res) => {
+    const {email, password} = req.body;
+    console.log(req.body);
+
+    const user = await Models.User.findOne({email: email});
+
+    if(!user) {
+        return res.status(404).json({
+            error: "Such user doesn't exist",
+        });
+    } else if(user.password !== password) {
+        return res.status(403).json({
+            error: "Invalid password",
+        });
+    } else {
+        delete user.password;
+
+        const payload = {}
+        const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m", audience: JSON.stringify(user) });
+        const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "1y", audience: JSON.stringify(user) });
+
+        res.json({ accessToken: accessToken, refreshToken: refreshToken });
+    }
+});
+
+app.post("/auth/register/", async (req, res) => {
+    const {email, username, password} = req.body;
+
+    const existingUser = await Models.User.findOne({email: email});
+    if(existingUser) {
+        res.status(400).json({error: "The user with such email already exists"})
+    } else {
+        const user = new Models.User({email: email, username: username, password: password});
+        await user.save();
+
+        const payload = {}
+        const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m", audience: JSON.stringify(user) });
+        const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "1y", audience: JSON.stringify(user) });
+
+        res.json({ accessToken: accessToken, refreshToken: refreshToken });
+    }
 });
--- models.js
+++ models.js
@@ -1,1 +1,30 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profilePic: {
+        data: Buffer,
+        contentType: String,
+    },
+    hasProfilePic: {
+        type: Boolean, // if no pfp, return some default image
+        default: false,
+    },
+    descr: String,
+});
+const User = mongoose.model('User', userSchema);
+
+module.exports = {User};
