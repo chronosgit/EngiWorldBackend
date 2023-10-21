1afabb655038c6de31449dcb598d82a09e0a0512
chronosgit <warfacemeister@mail.ru>
2023-10-21 22:48:54 +0600

    Added minor files renamings

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const verifyJWT = require("./middleware/verifyJWT");
 const refreshTokenHandler = require("./handlers/refreshTokenHandler");
 const userLoginHandler = require("./handlers/userLoginHandler");
 const userRegisterHandler = require("./handlers/userRegisterHandler");
-const userCRUDHandlers = require("./handlers/userCRUDHandlers");
-const postCRUDHandlers = require("./handlers/postCRUDHandlers");
+const userRUDHandlers = require("./handlers/userRUDHandlers");
+const postRUDHandlers = require("./handlers/postRUDHandlers");
 const postCreateHandler = require("./handlers/postCreateHandler");
 
 const Models = require("./models");
@@ -43,13 +43,13 @@ app.post("/auth/login/", userLoginHandler);
 app.post("/auth/register/", userRegisterHandler);
 
 app.route("/user/")
-    .get(verifyJWT, userCRUDHandlers.handleUserRead)
-    .put(verifyJWT, userCRUDHandlers.handleUserUpdate)
-    .delete(verifyJWT, userCRUDHandlers.handleUserDelete);
+    .get(verifyJWT, userRUDHandlers.handleUserRead)
+    .put(verifyJWT, userRUDHandlers.handleUserUpdate)
+    .delete(verifyJWT, userRUDHandlers.handleUserDelete);
 
 app.route("/post/:id/")
-    .get(verifyJWT, postCRUDHandlers.handlePostRead)
-    .put(verifyJWT, postCRUDHandlers.handlePostUpdate)
-    .delete(verifyJWT, postCRUDHandlers.handlePostDelete)
+    .get(verifyJWT, postRUDHandlers.handlePostRead)
+    .put(verifyJWT, postRUDHandlers.handlePostUpdate)
+    .delete(verifyJWT, postRUDHandlers.handlePostDelete)
 
 app.post("/post/create/", verifyJWT, postCreateHandler);
